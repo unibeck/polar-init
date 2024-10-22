f6078606aa144f692c6240f3df6c70c7fdcc1620
Emil Widlund <hello@emilwidlund.com>
2024-10-22 11:59:20 +0200

    update

---
+++ src/cli.tsx
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+import { render } from "ink";
+import meow from "meow";
+import React from "react";
+import App from "./app.js";
+
+const cli = meow(
+	`
+	Usage
+	  $ polar-init
+
+	Options
+		--org  Your organization name
+
+	Examples
+	  $ polar-init --org=my-org
+`,
+	{
+		importMeta: import.meta,
+		flags: {
+			name: {
+				type: "string",
+			},
+		},
+	},
+);
+
+render(<App name={cli.flags.name} />);
